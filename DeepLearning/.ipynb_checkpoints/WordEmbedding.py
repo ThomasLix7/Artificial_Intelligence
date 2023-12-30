2f2d1a9a12b3ed54f6ff974ab8a65de005b4ce3e
ThomasLix7 <lihongtaoix7@gmail.com>
2023-12-30 17:28:29 +0100

    DeepLearning Fundation

---
+++ DeepLearning/.ipynb_checkpoints/WordEmbedding.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence  import pad_sequences
+print(tf.__version__)
